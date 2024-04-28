496d60ea65833b8819a66fcd8a517863189a0cbd
pdLeal <lealpd@outlook.com>
2024-04-28 14:05:17 -0300

    started glitch effect at the header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Header_Style';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [isSelected, setIsSelected] = useState('home');
+  const menuItens = ['home', 'projects', 'about', 'contact'];
+
   return (
     <nav>
-        <S.Ul>
-            <Link to='/home'>Home</Link>
-            <Link to='/projects'>Projects</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/contact'>Contact</Link>
-        </S.Ul>
+      <S.Ul>
+        {menuItens.map((item) => {
+          if (isSelected === item) {
+            return (
+              <S.Li>
+                <Link className='glitch' to={`/${item}`}>
+                  <span aria-hidden='true'>{item}</span>
+                  {item}
+                  <span aria-hidden='true'>{item}</span>
+                </Link>
+              </S.Li>
+            );
+
+          } else {
+            return (
+              <S.Li onClick={() => setIsSelected(item)}>
+                <Link to={`/${item}`}>{item}</Link>
+              </S.Li>
+            );
+
+          }
+        })}
+
+        {/* <S.Li><Link className='glitch' to='/home'>Home</Link></S.Li>
+        <S.Li><Link to='/projects'>Projects</Link></S.Li>
+        <S.Li><Link to='/about'>About</Link></S.Li>
+        <S.Li><Link to='/contact'>Contact</Link></S.Li> */}
+      </S.Ul>
     </nav>
   )
 }
--- src/components/header/Header_Style.jsx
+++ src/components/header/Header_Style.jsx
@@ -1,30 +1,97 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const Ul = styled.ul`
     text-align: center;
     padding: 16px;
     max-width: 80vw;
     margin-inline: auto;
-    display: flex;
-    justify-content: space-evenly;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
     list-style: none;
     border-bottom: solid 1px var(--primary-color);
-    
+`;
+
+const glitchEffect = keyframes`
+     0% {
+    text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+      -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  14% {
+    text-shadow: 0.05em 0 0 rgba(255, 0, 0, 0.75),
+      -0.05em -0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  15% {
+    text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
+      0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  49% {
+    text-shadow: -0.05em -0.025em 0 rgba(255, 0, 0, 0.75),
+      0.025em 0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.05em -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  50% {
+    text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
+      0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  99% {
+    text-shadow: 0.025em 0.05em 0 rgba(255, 0, 0, 0.75),
+      0.05em 0 0 rgba(0, 255, 0, 0.75), 0 -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+  100% {
+    text-shadow: -0.025em 0 0 rgba(255, 0, 0, 0.75),
+      -0.025em -0.025em 0 rgba(0, 255, 0, 0.75),
+      -0.025em -0.05em 0 rgba(0, 0, 255, 0.75);
+  }
+`;
+
+export const Li = styled.li`
+    padding: 4px;
+
+        &:not(:last-child) { // Selects the li that are not the last child
+            border-right: solid 1px var(--primary-color);
+        }
+
         a {
-            flex-grow: 1;
             font-size: clamp(1.3rem, 2vw + 0.5rem, 2rem);text-decoration: none;
             color: white;
             transition: all 150ms linear;
-
-            position: relative;
-            z-index: 1;
-        }
-
-        a:not(:last-child) { // Selects the links that are not the last child
-            border-right: solid 1px var(--primary-color);
         }
 
         a:hover {
             text-shadow: 0px 0px 10px whitesmoke; 
+        }
+
+        a.glitch {
+            font-family: "VT323", monospace;
+            text-transform: uppercase;
+            text-shadow:
+                0.05em 0 0 rgba(255, 0, 0, 0.75),
+                -0.025em -0.05em 0 rgba(0, 255, 0, 0.75),
+                0.025em 0.05em 0 rgba(0, 0, 255, 0.75);
+            animation: ${glitchEffect} 750ms infinite;
+            position: relative;
+        }
+
+        .glitch span {
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+
+        .glitch span:first-child {
+            animation: glitch 650ms infinite;
+            clip-path: polygon(0 0, 100% 0, 100% 45%, 0 45%);
+            transform: translate(-0.05em, -0.025em);
+            opacity: 0.8;
+        }
+
+        .glitch span:last-child {
+            animation: glitch 375ms infinite;
+            clip-path: polygon(0 80%, 100% 20%, 100% 100%, 0 100%);
+            transform: translate(0.05em, 0.025em);
+            opacity: 0.8;
         }
 `;
