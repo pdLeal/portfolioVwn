0ba4e63a17df44007d0d636a92a05b1d5cf1ebac
pdLeal <lealpd@outlook.com>
2024-11-02 07:04:34 -0300

    warning and tease are displayed correctly

--- src/pages/denied/Denied.jsx
+++ src/pages/denied/Denied.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import * as S from './Denied_Style';
+import TypeEfct from '../../components/TypeEfct';
+
 
 function Denied() {
   return (
-    <S.Main> 
-      <S.H2 $direction='toRight'>!!!ACCESS DENIED!!!</S.H2>
-      <S.H2 $direction='toLeft'>!!!ACCESS DENIED!!!</S.H2>
+    <S.Main>
+      <S.H1>WARNING</S.H1>
+      <S.Container>
+        <S.H2 $direction='toRight'>!!!ACCESS DENIED!!!</S.H2>
+        <S.H2>!!!ACCESS DENIED!!!</S.H2>
+      </S.Container>
+      <S.Tease>
+        <TypeEfct text={['', 'Oh, c\'mon! You know you HAVE to complete the challenge to see what you want --"']} />
+      </S.Tease>
     </S.Main>
   )
 }
--- src/pages/denied/Denied_Style.jsx
+++ src/pages/denied/Denied_Style.jsx
@@ -1,8 +1,21 @@
 import styled, { keyframes, css } from "styled-components";
+import { H3 } from '../../components/hello/Hello_Style';
 
 export const Main = styled.main`
-    width: 100%;
     height: 100vh;
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    justify-items: center;
+    align-items: center;
+`;
+
+export const H1 = styled.h1`
+    font-size: clamp(2rem, 7vw + 1rem, 6rem);
+    color: red;
+`;
+
+export const Container = styled.main`
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -42,8 +55,15 @@ export const H2 = styled.h2`
         } else {
             return css`
                 animation: ${slideToLeft} var(--animation-duration) var(--animation-timing) forwards;
-                mask-image: linear-gradient(to top, black 50%, transparent 50%);
+                mask-image: linear-gradient(to top, black 0%, transparent 50%);
             `
         }
     }}         
+`;
+
+export const Tease = styled(H3)`
+    font-family: "VT323", monospace;
+    margin-inline: auto;
+    max-width: 80%;
+    text-align: center;
 `;
