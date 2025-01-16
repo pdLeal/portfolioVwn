655fba2c35e422583c958a359c2854a2cae5038d
pdLeal <lealpd@outlook.com>
2025-01-15 23:48:00 -0300

    PuzzleBoard has its own file

---
+++ src/components/puzzle/PuzzleBoard.jsx
@@ -0,0 +1,136 @@
+import React, { useRef, useState } from 'react'
+import * as S from './Puzzle_Style';
+import useBoardShuffler from '../../hooks/puzzle/useBoardShuffler';
+import checkCooldown from '../../utils/checkCooldown';
+import usePuzzleContext from '../../contexts/PuzzleContext';
+import useWinnerContext from '../../contexts/WinnerContext';
+import useSavePoint from '../../hooks/puzzle/useSavePoint';
+import handleErrors from '../../utils/handleErrors';
+import moveToEmpty from '../../utils/moveToEmpty';
+import useCheckMovement from '../../hooks/puzzle/useCheckMovement';
+
+import { Fireworks } from '@fireworks-js/react'
+
+function PuzzleBoard() {
+    const fireRef = useRef(null);
+  
+    const { projectWinner } = useWinnerContext();
+    const { canMove, setCanMove, hardModeIsOn } = usePuzzleContext();
+  
+    const { slots, shuffledPieces, pieceImg } = useBoardShuffler();
+    const { savePosition } = useSavePoint(fireRef);
+    const { checkCanMove } = useCheckMovement();
+  
+    const [lastClick, setLastClick] = useState(0);
+  
+    // Fireworks
+    const options = {
+      autoresize: true,
+      opacity: 0.2,
+      acceleration: 1,
+      friction: 0.99,
+      gravity: 1.5,
+      particles: 50,
+      traceLength: 3,
+      traceSpeed: 5,
+      explosion: 3,
+      intensity: 15,
+      flickering: 100,
+      lineStyle: 'round',
+      hue: {
+        min: 0,
+        max: 360
+      },
+      delay: {
+        min: 30,
+        max: 80
+      },
+      rocketsPoint: {
+        min: 0,
+        max: 100
+      },
+      lineWidth: {
+        explosion: {
+          min: 0.3,
+          max: 5
+        },
+        trace: {
+          min: 0.3,
+          max: 3
+        }
+      },
+      brightness: {
+        min: 100,
+        max: 100
+      },
+      decay: {
+        min: 0.005,
+        max: 0.015
+      },
+      mouse: {
+        click: false,
+        move: false,
+        max: 1
+      }
+    }
+  
+    return (
+      <S.Container $layout={4}>
+  
+        {slots.map((slot, i) => { // Allocates pieces to grid cells
+  
+          if (shuffledPieces[i] != '') {
+            return (
+              <div className='slot' data-empty='false' data-position={slot} key={slot}>
+                <S.Piece
+                  $imgUrl={pieceImg}
+                  data-piece={shuffledPieces[i]}
+                  onClick={e => {
+  
+                    try {
+                      checkCooldown(lastClick, setLastClick, 550, "Too many clicks!");
+                      hardModeIsOn && checkCanMove(e, canMove, setCanMove);
+  
+                    } catch (error) {
+                      handleErrors(e, error);
+                      return;
+                    }
+  
+                    moveToEmpty(e);
+  
+                    savePosition(e);
+  
+                  }}
+                >
+                </S.Piece>
+              </div>
+            )
+          } else {
+            return <div data-empty='true' data-position={slot} key={slot}></div>
+          }
+        })
+        }
+  
+        {projectWinner && <>
+          <Fireworks
+            ref={fireRef}
+            options={options}
+            style={{
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              position: 'absolute'
+            }}
+          />
+          <S.Congrats>YOU WIN!</S.Congrats>
+        </>}
+  
+        <S.ErrorMsg id='error'>
+          Hint: Clicking fast does not make you play better.
+        </S.ErrorMsg>
+      </S.Container>
+    )
+  }
+
+  export default PuzzleBoard
