6b9cf25883b994191f7ce39bc9ebbac14328b49b
pdLeal <lealpd@outlook.com>
2025-01-16 00:11:45 -0300

    PuzzleBoard refectored     still needs to refactor Puzzle comp

--- src/hooks/puzzle/useCheckMovement.jsx
+++ src/hooks/puzzle/useCheckMovement.jsx
@@ -3,28 +3,26 @@ import { useState } from "react";
 function useCheckMovement() {
     const [canMove, setCanMove] = useState(['12', '15']); // '6', '8'
     const checkCanMove = (e) => {
-
         const clickedElem = e.target;
-
         const positionInGrid = clickedElem.parentElement.dataset.position;
 
         if (!canMove.includes(positionInGrid)) {
-            throw new Error("This piece cannot move!"); // Impede que peças que não estão proximas se mexam
+            throw new Error("This piece cannot move!");
         }
 
-        // Determina as peças que podem se mover p/ o próximo espaço vazio
-        const numPosition = +positionInGrid;
-
-        const topSlot = `${numPosition - 4}`; // 4x4 GRID
-        const bottomSlot = `${numPosition + 4}`;
-        const leftSlot = `${(numPosition - 1) == 4 ||
-            (numPosition - 1) == 8 ||
-            (numPosition - 1) == 12 ? '' : numPosition - 1
-            }`;
-        const rightSlot = `${(numPosition + 1) == 5 ||
-            (numPosition + 1) == 9 ||
-            (numPosition + 1) == 13 ? '' : numPosition + 1
-            }`;
+        // Determines which pieces can move to the empty space
+        const topSlot = `${+positionInGrid - 4}`;
+        const bottomSlot = `${+positionInGrid + 4}`;
+        const leftSlot = `${(+positionInGrid - 1) == 4 ||
+                            (+positionInGrid - 1) == 8 ||
+                            (+positionInGrid - 1) == 12 ? ''
+                                                        : +positionInGrid - 1
+                            }`;
+        const rightSlot = `${(+positionInGrid + 1) == 5 ||
+                             (+positionInGrid + 1) == 9 ||
+                             (+positionInGrid + 1) == 13 ? ''
+                                                        : +positionInGrid + 1
+                            }`;
         const tempArray = [topSlot, bottomSlot, leftSlot, rightSlot];
 
         setCanMove(tempArray);
@@ -36,15 +34,15 @@ function useCheckMovement() {
 
 export default useCheckMovement
 // // Determina as peças que podem se mover p/ o próximo espaço vazio numa grid 3x3
-// // const topSlot = `${numPosition - 3}`; 3x3 GRID
-// // const bottomSlot = `${numPosition + 3}`;
+// // const topSlot = `${positionInGrid - 3}`; 3x3 GRID
+// // const bottomSlot = `${positionInGrid + 3}`;
 // // const leftSlot = `${
-// //                     (numPosition - 1) == 0 ||
-// //                     (numPosition - 1) == 3 ||
-// //                     (numPosition - 1) == 6 ? '' : numPosition - 1
+// //                     (positionInGrid - 1) == 0 ||
+// //                     (positionInGrid - 1) == 3 ||
+// //                     (positionInGrid - 1) == 6 ? '' : positionInGrid - 1
 // //                   }`;
 // // const rightSlot = `${
-// //                     (numPosition + 1) == 4 ||
-// //                     (numPosition + 1) == 7 ||
-// //                     (numPosition + 1) == 10 ? '' : numPosition + 1
+// //                     (positionInGrid + 1) == 4 ||
+// //                     (positionInGrid + 1) == 7 ||
+// //                     (positionInGrid + 1) == 10 ? '' : positionInGrid + 1
 // //                   }`;
