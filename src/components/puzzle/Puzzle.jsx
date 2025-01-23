e746f2ff90e795efe24281223cdf817c3b0636fd
pdLeal <lealpd@outlook.com>
2025-01-23 07:53:54 -0300

    now it's really fixed!     i hope

--- src/components/puzzle/Puzzle.jsx
+++ src/components/puzzle/Puzzle.jsx
@@ -4,17 +4,27 @@ import * as S from './Puzzle_Style';
 import{ PuzzleProvider } from '../../contexts/PuzzleContext';
 import useWinnerContext from '../../contexts/WinnerContext';
 import { useTranslation } from 'react-i18next';
-import useRestart from '../../hooks/puzzle/useRestart';
 import useHardMode from '../../hooks/puzzle/useHardMode';
 
 function Puzzle() {
   const [savedPiecesPosition, setSavedPiecesPosition] = useState([]);
   const [projectUrl, setProjectUrl] = useState('');
-  const { projectWinner } = useWinnerContext();
+  const { projectWinner, setProjectWinner } = useWinnerContext();
 
-
-  const { reloadPuzzleBoard, handleRestart } = useRestart();
   const { hardModeIsOn, handleHardMode } = useHardMode();
+
+  const [reloadPuzzleBoard, setReloadPuzzleBoard] = useState(true); // true/false doens't mattter, just needs to change to remount the whole comp
+
+  function handleRestart() {
+      setReloadPuzzleBoard(!reloadPuzzleBoard);
+  
+      localStorage.removeItem('piecesPosition');
+      localStorage.removeItem('canMove');
+      setProjectWinner(false);
+      setSavedPiecesPosition([]);
+      
+      // For some reason that I cannot comprehend, putting handleRestar as a custom hook/own file causes the shuffler to go bananas, preventing the user to win unless he/she reloads the entire page. Future me, please try to understand this when you have more knowlodge!
+  }
 
   // I18NEXT
   const { t } = useTranslation();
